3f8d8631b7b116b14b1c3467099f14eb1fc9e827
Olga Shikunova <88195636+Olshyk@users.noreply.github.com>
2022-04-19 01:39:23 +0300

    Add files via upload

--- 1.5/js/main.js
+++ 1.5/js/main.js
@@ -1,12 +1,16 @@
-const showLink = document.querySelector('.link');
-const infoList = document.querySelector('.info__list');
+function more() {
+	const showLink = document.querySelector('.link');
+	const infoList = document.querySelector('.info__list');
 
-function showMore(link, info) {
-  showLink.addEventListener('click', () => {
-    showLink.classList.toggle('link--hide');
-    showLink.textContent = showLink.textContent === 'Показать все' ? 'Скрыть' : 'Показать все';
-    infoList.classList.toggle('info__list--full');
-  })
+	function showMore(link, info) {
+		showLink.addEventListener('click', () => {
+			showLink.classList.toggle('link--hide');
+			showLink.textContent = showLink.textContent === 'Показать все' ? 'Скрыть' : 'Показать все';
+			infoList.classList.toggle('info__list--full');
+		});
+	}
+
+	showMore(showLink, infoList);
 }
 
-showMore(showLink, infoList);
+more();
--- 1.5/js/swiper.js
+++ 1.5/js/swiper.js
@@ -1,30 +1,29 @@
 function initSlider() {
-  new Swiper('.swiper', {
-    loop: true,
-    slidesPerView: 1.25,
-    spaceBetween: 16,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    breakpoints: {
-     375: {
-        slidesPerView: 1.5,
-      },
-     450: {
-        slidesPerView: 1.75,
-      },
-     540: {
-        slidesPerView: 2.1,
-      },
-     620: {
-        slidesPerView: 2.25,
-      },
-    }
-  });
-  }
- 
-  
-if(window.innerWidth < 768) {
-  initSlider()
+	new Swiper('.swiper', {
+		loop: true,
+		slidesPerView: 1.25,
+		spaceBetween: 16,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		breakpoints: {
+			375: {
+				slidesPerView: 1.5,
+			},
+			450: {
+				slidesPerView: 1.75,
+			},
+			540: {
+				slidesPerView: 2.1,
+			},
+			620: {
+				slidesPerView: 2.25,
+			},
+		},
+	});
+}
+
+if (window.innerWidth < 768) {
+	initSlider();
 }
